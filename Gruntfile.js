5245ff4bfdee09e91d8af1d11231efb6b8e72ae4
Nick Tomlin <nick.tomlin@gmail.com>
2013-06-28 13:50:04 -0500

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'js/lib/columns.js',
+        dest: 'js/lib/columns.min.js'
+      }
+    },
+    stylus: {
+      compile: {
+        options: {
+          paths: ['stylus']
+         },
+         files: {
+          'static/css/main.css': ['stylus/main.styl'] // compile and concat into single file
+       }
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 3020
+        }
+      },
+      preview: {
+        options: {
+          port: 3020
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['js'],
+        tasks: ['uglify']
+      },
+      styl: {
+        files: ['stylus/*.styl'],
+        tasks: ['stylus']
+      },
+      livereload: {
+          files: ['static/css/*.css'],
+          options: {
+            livereload: true
+          }
+      }
+    }
+  });
+
+  // contrib
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  // default
+  grunt.registerTask('default', ['connect:preview','watch']);
+
+};
