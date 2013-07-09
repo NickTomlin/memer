3bbac3ada469a9ee9b2e72e96b2d40237a48424e
Nick Tomlin <nick.tomlin@gmail.com>
2013-07-09 16:48:21 -0500

    indentation

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,21 @@
 requirejs(['require-config'], function () {
+  // Load the main app module to start the app
+  require(["lodash" ,  "memer"], function(_, memer){
+    contentEle = document.getElementById('content');
 
-// Load the main app module to start the app
-require(["lodash" ,  "memer"], function(_, memer){
-  contentEle = document.getElementById('content');
-
-  if ( memer.hasGetUserMedia() ) {
-    var yayText = document.querySelector('.yay');
-    yayText.classList.toggle('is-hidden');
-
-    var buttonStart = document.getElementById('start');
-    buttonStart.addEventListener('click', function(){
-      memer.init(contentEle);
+    if ( memer.hasGetUserMedia() ) {
+      var yayText = document.querySelector('.yay');
       yayText.classList.toggle('is-hidden');
-    });
-  } else {
-    nayText = document.querySelector('.nay');
-    nayText.classList.toggle('is-hidden');
-  }
 
-});
+      var buttonStart = document.getElementById('start');
+      buttonStart.addEventListener('click', function(){
+        memer.init(contentEle);
+        yayText.classList.toggle('is-hidden');
+      });
+    } else {
+      nayText = document.querySelector('.nay');
+      nayText.classList.toggle('is-hidden');
+    }
+
+  });
 });
