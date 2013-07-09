57a5c42f9f78f7082db23de2716cc8bcb47daada
Nick Tomlin <nick.tomlin@gmail.com>
2013-07-09 16:47:52 -0500

    update grunt build task for optim and copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,25 +40,35 @@ module.exports = function(grunt) {
     requirejs: {
       compile: {
         options: {
-          name: "main",
-          MainConfigFile: "app/js/require-config.js",
-          optimize: "none",
-          findNestedDependencies: false,
-          baseUrl: "app/js",
-          out: "build/opt.js"
+          mainConfigFile: "app/js/require-config.js", // this config is shared by the optimizer and our main.js module
+          baseUrl: "app/js", // where r.js should start looking for modules
+          name: "main", // kickoff file
+          include: ["memer", "lodash"], // @todo, have r.js grok this from app/js file instead of redclaring
+          out: "build/js/main.js"
         }
+      }
+    },
+    copy: {
+      app: {
+        files: [
+          {expand: true, cwd:'app', src: "static/**", dest: "dist"},
+          {expand: true, flatten: true, src:'app/*.html', dest: "dist"},
+          {expand: true, cwd: 'app/js', src: 'vendor/requirejs/require.js', dest:"dist/js" },
+          {expand: true, cwd:'build', src: 'js/*', dest: "dist"} // optimized by require (does not include require)
+        ]
       }
     }
   });
 
   // contrib
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-stylus');
   grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('build', ['requirejs']);
-  // default
+  grunt.registerTask('build', ['requirejs', 'copy']); // add a copy + min task here
+
   grunt.registerTask('default', ['stylus:compile','connect','watch']);
 
 };
